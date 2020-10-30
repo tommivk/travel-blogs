a47558b3ccf8bbfd831697fea05f9757853e1a8c
Tommi Kokkonen <tommikokk@gmail.com>
2020-10-30 07:07:17 +0200

    add users controller

---
+++ backend/controllers/users.js
@@ -0,0 +1,21 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/', async (req, res) => {
+  const body = req.body
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+  const user = new User({
+    username: body.username,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+  console.log(savedUser)
+  res.json(savedUser)
+})
+
+module.exports = usersRouter
--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,11 @@ const express = require('express')
 const app = express()
 const PORT = 8008
 const cors = require('cors')
+const usersRouter = require('./controllers/users')
 app.use(cors())
+app.use(express.json())
+
+app.use('/api/users', usersRouter)
 
 app.get('/ping', (req, res) => {
   res.send('pong')
