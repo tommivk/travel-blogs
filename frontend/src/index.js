2acb664d0a9a7d93c9ca59e15319ab367ccac0e9
Tommi Kokkonen <tommikokk@gmail.com>
2020-10-30 09:15:20 +0200

    add login to frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,62 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  return <div>Hello, world!</div>
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const loggedUser = localStorage.getItem('loggedTravelBlogUser')
+    if (loggedUser) {
+      setUser(JSON.parse(loggedUser))
+    }
+  }, [])
+
+  return (
+    <div>
+      {user === null ? 'Hello, world!' : `Hello ${user.username}`}
+      {!user && <Login setUser={setUser}></Login>}
+    </div>
+  )
+}
+
+const Login = ({ setUser }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    try {
+      const user = await axios.post('http://localhost:8008/api/login', {
+        username: username,
+        password: password,
+      })
+      setUsername('')
+      setPassword('')
+      setUser(user.data)
+      console.log(user.data)
+      window.localStorage.setItem(
+        'loggedTravelBlogUser',
+        JSON.stringify(user.data)
+      )
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <input
+          type='text'
+          onChange={({ target }) => setUsername(target.value)}
+        ></input>
+        <input
+          type='password'
+          onChange={({ target }) => setPassword(target.value)}
+        ></input>
+        <button type='submit'>Login</button>
+      </form>
+    </div>
+  )
 }
 
 ReactDOM.render(
