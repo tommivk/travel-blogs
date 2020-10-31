649d7c04f70df1242a37cca2d0e7c3446856dd08
Tommi Kokkonen <tommikokk@gmail.com>
2020-10-31 04:31:10 +0200

    configure firebase

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,23 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const admin = require('firebase-admin')
+//GOOGLE_APPLICATION_CREDENTIALS="/path/credentials.json"
 const PORT = 8008
 const cors = require('cors')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
-
 const MongoURI = process.env.MONGO_DB_URI
 mongoose.connect(MongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true,
+})
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: 'https://travelblogs-ac98f.firebaseio.com',
 })
 
 app.use(cors())
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,20 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
+import firebase from 'firebase/app'
+require('firebase/auth')
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FB_DATABASE_URL,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FB_APP_ID,
+}
+
+firebase.initializeApp(firebaseConfig)
 
 const App = () => {
   const [user, setUser] = useState(null)
