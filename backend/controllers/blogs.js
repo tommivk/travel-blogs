0dd9ca8f6537cee38804b04883a89e568530cc33
Tommi Kokkonen <tommikokk@gmail.com>
2020-11-01 16:17:34 +0200

    get route for all blogs

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -2,6 +2,12 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const blogsRouter = require('express').Router()
 
+blogsRouter.get('/', async (req, res) => {
+  console.log('asd')
+  const blogs = await Blog.find({})
+  res.json(blogs.map((blog) => blog.toJSON()))
+})
+
 blogsRouter.post('/', async (req, res) => {
   const body = req.body
   const user = await User.findOne({ username: body.username })
