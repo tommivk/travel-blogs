5ebbf59c9746bd94eeed1af009ddd960e5833631
Tommi Kokkonen <tommikokk@gmail.com>
2020-11-01 10:34:42 +0200

    add blogSchema

---
+++ backend/models/blog.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  date: Date,
+  content: String,
+  location: {
+    lat: { type: Number },
+    lng: { type: Number },
+  },
+  stars: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  headerImageURL: String,
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+module.exports = Blog
