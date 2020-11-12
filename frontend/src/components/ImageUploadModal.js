99d3436db3a74b44b909cdf538529d9512a4b757
Tommi Kokkonen <tommikokk@gmail.com>
2020-11-12 16:06:44 +0200

    add styles

--- frontend/src/components/ImageUploadModal.js
+++ frontend/src/components/ImageUploadModal.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import firebase from 'firebase/app'
 import axios from 'axios'
-import { Button, Modal, Input } from '@material-ui/core'
+import { Button, Modal, Input, LinearProgress } from '@material-ui/core'
+import imageModalBG from '../images/imagemodalbg.jpg'
 
 const ImageUploadModal = ({
   user,
@@ -13,11 +14,18 @@ const ImageUploadModal = ({
   const [image, setImage] = useState(null)
   const [imagePreview, setImagePreview] = useState(null)
   const [uploadedImages, setUploadedImages] = useState([])
+  const [uploadProgress, setUploadProgress] = useState(0)
   console.log(uploadModalOpen)
 
   const handleImageChange = (e) => {
     setImage(e.target.files[0])
     setImagePreview(URL.createObjectURL(e.target.files[0]))
+    URL.revokeObjectURL(e.target.files[0])
+  }
+
+  const cancelImage = () => {
+    setImage(null)
+    setImagePreview(null)
   }
 
   const uploadPicture = async (uploadedPictureURL) => {
@@ -36,7 +44,7 @@ const ImageUploadModal = ({
         }
       )
       const newUser = user
-      newUser.pictures = user.pictures.concat(response.data)
+      newUser.pictures = [response.data].concat(user.pictures)
       setUser(newUser)
       setUploadedImages([uploadedPictureURL].concat(uploadedImages))
       window.localStorage.setItem('loggedTravelBlogUser', JSON.stringify(user))
@@ -61,6 +69,7 @@ const ImageUploadModal = ({
       (snapshot) => {
         let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
         console.log('Upload is ' + progress + '% done')
+        setUploadProgress(progress)
         switch (snapshot.state) {
           case firebase.storage.TaskState.PAUSED:
             console.log('Upload is paused')
@@ -81,6 +90,7 @@ const ImageUploadModal = ({
     )
   }
   if (!uploadModalOpen) return null
+
   return (
     <div>
       <Modal
@@ -95,25 +105,69 @@ const ImageUploadModal = ({
             top: '50%',
             left: '50%',
             transform: `translate(-50%, -50%)`,
-            backgroundColor: 'white',
             width: '60%',
             height: '60%',
+            backgroundImage: `url(${imageModalBG})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundPosition: 'left bottom',
+            color: 'white',
           }}
         >
-          <h2 style={{ textAlign: 'center' }}>Upload Images</h2>
+          <h2
+            style={{
+              textAlign: 'center',
+            }}
+          >
+            <span
+              style={{
+                width: 'fit-content',
+                backgroundColor: 'rgb(0,0,0)',
+                padding: '8px',
+                borderRadius: '5px',
+              }}
+            >
+              Upload Images
+            </span>
+          </h2>
+          <div style={{ position: 'absolute', top: '20%', left: '42%' }}>
+            <div
+              style={{
+                border: '3px solid black',
+                width: '300px',
+                height: '300px',
+                backgroundColor: 'rgba(0,0,0,0.7)',
+              }}
+            >
+              {imagePreview && (
+                <img alt='' src={imagePreview} height='300' width='300'></img>
+              )}
+            </div>
 
-          <div style={{ position: 'absolute', left: '5%', bottom: '5%' }}>
-            {imagePreview && (
-              <img alt='' src={imagePreview} height='200' width='200'></img>
-            )}
+            <LinearProgress variant='determinate' value={uploadProgress} />
 
-            <form onSubmit={handleImageUpload}>
-              <input type='file' onChange={handleImageChange}></input>
-
-              <Button type='submit' variant='contained' color='secondary'>
-                upload
-              </Button>
-            </form>
+            <div
+              style={{ position: 'relative', textAlign: 'center', top: '10px' }}
+            >
+              <form onSubmit={handleImageUpload}>
+                {image ? (
+                  <div>
+                    <Button
+                      onClick={cancelImage}
+                      variant='contained'
+                      style={{ marginRight: '5px' }}
+                    >
+                      Cancel
+                    </Button>
+                    <Button type='submit' variant='contained' color='secondary'>
+                      upload
+                    </Button>
+                  </div>
+                ) : (
+                  <input type='file' onChange={handleImageChange}></input>
+                )}
+              </form>
+            </div>
           </div>
           <div
             style={{
@@ -122,14 +176,15 @@ const ImageUploadModal = ({
               top: '3%',
               width: '25%',
               height: '94%',
-              overflowY: 'scroll',
+              // overflowY: 'scroll',
               textAlign: 'center',
               border: '2px solid black',
               borderRadius: '2%',
-              overflowX: 'hidden',
+              overflow: 'hidden',
+              backgroundColor: '#1d1f1e',
             }}
           >
-            <h4>My Images</h4>
+            <h4>My Images ({user.pictures.length})</h4>
             <div
               style={{
                 display: 'flex',
@@ -139,10 +194,11 @@ const ImageUploadModal = ({
                 flexWrap: 'wrap',
               }}
             >
-              {user.pictures.map((picture, i) => (
+              {user.pictures.length === 0 && <p>No images uploaded.</p>}
+              {user.pictures.map((picture) => (
                 <div>
                   <img
-                    key={i}
+                    key={picture.id}
                     alt=''
                     src={picture.imgURL}
                     height='100'
