e4aabf1c60fb3931c1d2c72a4ba6c72768c0ea63
Tommi Kokkonen <tommikokk@gmail.com>
2020-11-14 00:33:22 +0200

    add styles

--- frontend/src/components/SinglePicturePage.js
+++ frontend/src/components/SinglePicturePage.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Container } from '@material-ui/core'
+import ArrowUpward from '@material-ui/icons/ArrowUpward'
+import ArrowDownward from '@material-ui/icons/ArrowDownward'
 
 const SinglePicturePage = ({ picture, allPictures }) => {
   console.log('asdASDa')
@@ -43,7 +45,9 @@ const SinglePicturePage = ({ picture, allPictures }) => {
             {pictureIndex - 1 >= 0 && (
               <Link to={`/gallery/${allPictures[pictureIndex - 1].id}`}>
                 {' '}
-                <button>Previous</button>
+                <Button color='primary' variant='contained'>
+                  Previous
+                </Button>
               </Link>
             )}
           </div>
@@ -56,7 +60,9 @@ const SinglePicturePage = ({ picture, allPictures }) => {
             {allPictures.length > pictureIndex + 1 && (
               <Link to={`/gallery/${allPictures[pictureIndex + 1].id}`}>
                 {' '}
-                <button>Next</button>
+                <Button color='primary' variant='contained'>
+                  Next
+                </Button>
               </Link>
             )}
           </div>
@@ -68,6 +74,29 @@ const SinglePicturePage = ({ picture, allPictures }) => {
 
       <div
         style={{
+          position: 'absolute',
+          top: '20%',
+          color: 'white',
+          left: '26%',
+          width: '3%',
+          height: '12%',
+          backgroundColor: '#231A03',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-around',
+          alignItems: 'center',
+        }}
+      >
+        <div>
+          <ArrowUpward></ArrowUpward>
+        </div>{' '}
+        <div>33</div>
+        <div>
+          <ArrowDownward></ArrowDownward>
+        </div>
+      </div>
+      <div
+        style={{
           height: '40%',
           width: '15%',
           backgroundColor: '#231A03',
@@ -75,9 +104,20 @@ const SinglePicturePage = ({ picture, allPictures }) => {
           right: '8%',
           top: '10%',
           color: 'white',
+          textAlign: 'center',
+          borderRadius: '4%',
         }}
       >
-        uploaded by
+        <div style={{ margin: '4%' }}>
+          <img src={picture.user.avatar} width='70%'></img> 
+        </div>
+        <div>
+          <h2>{picture.user.username}</h2>
+        </div>
+        <div>
+          <h4>Uploaded:</h4> {picture.date.toString()}
+        </div>
+        <div></div>
       </div>
     </div>
   )
