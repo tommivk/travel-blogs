e3d6e74f3df00581faf0c47c17ae5e4147970341
Tommi Kokkonen <tommikokk@gmail.com>
2020-12-03 15:02:55 +0200

    notificationSchema

---
+++ backend/models/notification.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const notificationSchema = mongoose.Schema({
+  sender: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+  },
+  receivers: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  readBy: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  message: String,
+  createdAt: Date,
+})
+
+const Notification = mongoose.model('Notification', notificationSchema)
+
+module.exports = Notification
