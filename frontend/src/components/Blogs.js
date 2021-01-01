c3eb42e8d1766b0e484276a9fefff73e08a63215
tommivk <tommikokk@gmail.com>
2021-01-01 18:48:33 +0200

    blog card style changes

--- frontend/src/components/Blogs.js
+++ frontend/src/components/Blogs.js
@@ -23,8 +23,8 @@ const Blogs = ({ allBlogs }) => {
 
   useEffect(() => {
     setBlogs(allBlogs)
-  },[allBlogs])
-   
+  }, [allBlogs])
+
   useEffect(() => {
     if (blogs && allBlogs) {
       let sortedBlogs = allBlogs.slice()
@@ -102,10 +102,7 @@ const Blogs = ({ allBlogs }) => {
                   blog.headerImageURL && (
                     <SwiperSlide>
                       <Link to={`/blogs/${blog.id}`}>
-                        <img
-                          src={blog.headerImageURL}
-                          width="80%"
-                        ></img>
+                        <img src={blog.headerImageURL} width="80%"></img>
                         <div className="swiper-blog-info">
                           <div className="swiper-blog-title">{blog.title}</div>
                           <div className="swiper-blog-description">
@@ -144,8 +141,11 @@ const Blogs = ({ allBlogs }) => {
             {param.country && <h1>Blogs about {param.country}</h1>}
           </div>
           <div className="blog-filter-selection">
-            Sort By{' '}
+            <div>
+            Sort By
+            </div>
             <Select
+              id={"blog-filter-select"}
               style={{ color: 'white' }}
               onChange={({ target }) => setSortBy(target.value)}
               value={sortBy}
@@ -158,33 +158,38 @@ const Blogs = ({ allBlogs }) => {
         <div className="blogs-container">
           <div className="cards-container">
             {blogs.map((blog) => (
-              <div key={blog.id}>             
-               <Link id="main-blog-link" to={`/blogs/${blog.id}`}>
-                <div className="blog-card">
-                  <div className="blog-header">
-                    <div className="blog-title">
-                      <h1>{blog.title}</h1>
+              <div key={blog.id}>
+                <Link id="main-blog-link" to={`/blogs/${blog.id}`}>
+                  <div className="blog-card">
+                    <div className="blog-header">
+                      <div className="blog-title">
+                        <h1>{blog.title.toUpperCase()}</h1>
+                      </div>
+                      <div className="blog-author-username">
+                       Written by {blog.author.username}
+                      </div>
                     </div>
-                    <div className="blog-author-username">
-                      Written by {blog.author.username}
+                    <div className="blog-image">
+                      {blog.headerImageURL && (
+                        <img src={blog.headerImageURL} width="300px"></img>
+                      )}
                     </div>
-                  </div>
-                  <div className="blog-image">
-                    <img src={blog.headerImageURL} width="300px"></img>
-                  </div>
 
-                  <div className="blog-description">
-                    <h4>{blog.description}</h4>
-                  </div>
-                  <div className="blog-star">
-                    <div className="tooltip">
-                    <span className="tooltip-message">Stars</span>
-                    <Star id="star" fontSize="default"></Star>
+                    <div className="blog-description">
+                      <h4>{blog.description}</h4>
                     </div>
-                    <div id="blog-stars-count">{blog.stars.length}</div>
+                    <div className="blog-star">
+                      <div className="tooltip">
+                        <span className="tooltip-message">Stars</span>
+                        <Star id="star" fontSize="default"></Star>
+                      </div>
+                      <div id="blog-stars-count">{blog.stars.length}</div>
+                    </div>
+                    <div className="blog-date">
+                        {DateTime.fromISO(blog.date).monthShort} {DateTime.fromISO(blog.date).day}
+                    </div>
                   </div>
-                </div>
-              </Link>
+                </Link>
               </div>
             ))}
             <div className="blog-pseudo-element"></div>
