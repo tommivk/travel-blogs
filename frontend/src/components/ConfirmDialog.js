2bbffb411939da5350c1e3c5c7f254502b8f0978
tommivk <tommikokk@gmail.com>
2021-02-10 20:13:44 +0200

    add loading circle to ConfirmDialog

--- frontend/src/components/ConfirmDialog.js
+++ frontend/src/components/ConfirmDialog.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -7,32 +7,53 @@ import Warning from '@material-ui/icons/Warning';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 import '../styles/confirmDialog.css';
 
 const ConfirmDialog = ({
   dialogTitle, dialogText, dialogOpen, handleDialogConfirm, handleDialogClose,
-}) => (
-  <Dialog className="confirm-dialog" open={dialogOpen} onClose={handleDialogClose}>
-    <div className="confirm-dialog-main-content">
-      <DialogTitle>{dialogTitle}</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          <div className="confirm-dialog-text">
-            {dialogText !== ''
+}) => {
+  const [showLoadingCircle, setShowLoadingCircle] = useState(false);
+
+  useEffect(() => {
+    setShowLoadingCircle(false);
+  }, [dialogOpen]);
+
+  const handleConfirm = () => {
+    setShowLoadingCircle(true);
+    handleDialogConfirm();
+  };
+
+  return (
+    <Dialog className="confirm-dialog" open={dialogOpen} onClose={handleDialogClose}>
+      <div className="confirm-dialog-main-content">
+        <DialogTitle>{dialogTitle}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <div className="confirm-dialog-text">
+              {dialogText !== ''
             && <Warning id="confirm-dialog-warning-icon" />}
-            {dialogText}
+              {dialogText}
+            </div>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <div className="confirm-dialog-buttons">
+            {showLoadingCircle
+              ? <CircularProgress id="confirm-dialog-spinner" />
+              : (
+                <>
+                  <Button id="confirm-dialog-ok-button" onClick={handleConfirm}>OK</Button>
+                  <Button id="confirm-dialog-cancel-button" onClick={handleDialogClose}>Cancel</Button>
+                </>
+              )}
           </div>
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <div className="confirm-dialog-buttons">
-          <Button id="confirm-dialog-ok-button" onClick={handleDialogConfirm}>OK</Button>
-          <Button id="confirm-dialog-cancel-button" onClick={handleDialogClose}>Cancel</Button>
-        </div>
-      </DialogActions>
-    </div>
-  </Dialog>
-);
+        </DialogActions>
+      </div>
+    </Dialog>
+  );
+};
 
 ConfirmDialog.propTypes = {
   dialogTitle: PropTypes.string.isRequired,
