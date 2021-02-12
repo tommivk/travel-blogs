5e8273a4186520e9f7bc127c6413deb01d69b5b9
tommivk <tommikokk@gmail.com>
2021-02-12 15:20:54 +0200

    hide location toggle button on small screens

--- frontend/src/components/SingleBlogPage.js
+++ frontend/src/components/SingleBlogPage.js
@@ -196,7 +196,7 @@ const SingleBlogPage = ({
          <button id="blog-locations-close-button" type="button" onClick={() => setShowLocations(false)}>X</button>
        </div>
        )}
-      <Container maxWidth="md">
+      <Container maxWidth="md" className="blog-container">
         <div>
           <div style={{ textAlign: 'center' }}>
             <h1 id="blog-title">{blog.title}</h1>
