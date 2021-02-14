20f27adc31bb667f948d264f18c2a1924182c08b
tommivk <tommikokk@gmail.com>
2021-02-14 18:58:21 +0200

    add tests for gallery pictures

--- frontend/cypress/integration/travelblogs.spec.js
+++ frontend/cypress/integration/travelblogs.spec.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable max-len */
 /* eslint-disable func-names */
 /* eslint-disable prefer-arrow-callback */
 import 'cypress-iframe';
@@ -108,5 +110,91 @@ describe('User can create new blog', function () {
         cy.contains('Description for test blog').should('not.exist');
       });
     });
+  });
+});
+
+describe('Gallery uploading', function () {
+  it('User can upload image to gallery', function () {
+    cy.get('[href="/gallery"] > .header-link > h1').click();
+    cy.get('.gallery-upload-images-button').click();
+    cy.fixture('../images/testpicture.jpg').as('picture');
+
+    cy.get('input[type=file]').then(function (el) {
+      const blob = Cypress.Blob.base64StringToBlob(this.picture, 'image/jpg');
+
+      const file = new File([blob], 'images/picture.jpg', { type: 'image/jpg' });
+      const list = new DataTransfer();
+
+      list.items.add(file);
+      const myFileList = list.files;
+
+      el[0].files = myFileList;
+      el[0].dispatchEvent(new Event('change', { bubbles: true }));
+    });
+
+    cy.get('#upload-modal-next-button-first').click();
+    cy.get('#image-upload-title-input').type('Test title');
+    cy.get('#upload-modal-next-button').click();
+    cy.get('#upload-modal-search-input').type('helsinki');
+    cy.get('#upload-modal-search-button').click();
+    cy.contains('Helsinki');
+    cy.contains('Finland');
+    cy.get('.location-result-select-button').click();
+    cy.contains('Publish This Image To Gallery?');
+    cy.get('#upload-modal-yes-button').click();
+    cy.contains('Preview');
+    cy.contains('Image Info');
+    cy.contains('Title:');
+    cy.contains('Test title');
+    cy.contains('Public');
+    cy.contains('Yes');
+    cy.contains('Location');
+    cy.contains('Country:');
+    cy.contains('Finland');
+    cy.contains('Helsinki');
+    cy.get('#image-modal-upload-button').click();
+    cy.get('body').click(4, 50);
+    cy.contains('Test title');
+    cy.get('.gallery-card').click();
+    cy.contains('Test title');
+  });
+
+  it('Location info is shown', function () {
+    cy.get('.image-toggle-button').click();
+    cy.get('.picture-container img').should('be.visible');
+    cy.contains('Helsinki');
+    cy.contains('Finland');
+    cy.get('#picture-location-info-map-icon').should('be.visible');
+  });
+
+  it('User can post new comment', function () {
+    cy.get('.comment-input-field').type('new comment');
+    cy.get('.comment-submit-button').click();
+    cy.contains('new comment');
+    cy.contains('testuser');
+    cy.contains('less than a minute ago');
+  });
+
+  it('User can delete comment', function () {
+    cy.get('.comment-delete-button').click();
+    cy.get('#confirm-dialog-ok-button').click();
+    cy.contains('new comment').should('not.exist');
+  });
+
+  it('Picture is shown on user page', function () {
+    cy.get('#header-user-avatar').click();
+    cy.get('#my-page-menulink').click();
+    cy.get('#userpage-pictures-radio').click();
+    cy.contains('Test title');
+    cy.get('.gallery-card > img').should('be.visible');
+  });
+
+  it('User can delete picture', function () {
+    cy.get('#userpage-picture-delete-button').click();
+    cy.get('#confirm-dialog-ok-button').click();
+    cy.contains('Test title').should('not.exist');
+    cy.get('.gallery-card > img').should('not.exist');
+    cy.contains('My Uploaded Pictures');
+    cy.contains('No pictures uploaded yet');
   });
 });
